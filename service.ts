a63fa130110187b821b1b45e691a7dc936fde1bd
Phan Phuc Khang <rhoneyrvonrv882@hotmail.com>
2025-04-10 08:40:57 +0700

    Create service.ts

---
+++ service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  private api = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.api}/login`, { email, password });
+  }
+
+  register(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.api}/register`, { email, password });
+  }
+
+  saveToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getToken();
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+}
